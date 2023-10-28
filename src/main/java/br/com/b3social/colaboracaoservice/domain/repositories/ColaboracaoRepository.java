2530d804687c9ac14dea1cd843026b67f3157c3b
Roberth Fontes Ovelar <101338393+RoberthOvelar@users.noreply.github.com>
2023-10-28 02:39:09 -0400

    refac: Especificando detalhes de busca

--- src/main/java/br/com/b3social/colaboracaoservice/domain/repositories/ColaboracaoRepository.java
+++ src/main/java/br/com/b3social/colaboracaoservice/domain/repositories/ColaboracaoRepository.java
@@ -1,6 +1,8 @@
 package br.com.b3social.colaboracaoservice.domain.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import br.com.b3social.colaboracaoservice.domain.models.Colaboracao;
 
@@ -8,7 +10,8 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ColaboracaoRepository extends JpaRepository<Colaboracao, String>{
-    Optional<Integer> countByAcaoSocialId(String acaoSocialId);
+    @Query("SELECT COUNT(e) FROM colaboracao e WHERE e.acaoSocialId = :acaoSocialId AND e.status = 'INSCRICAO_DEFERIDA'")
+    Optional<Integer> countByAcaoSocialId(@Param("acaoSocialId") String acaoSocialId);
     List<Colaboracao> findByAcaoSocialIdAndCoordenadorId(String acaoSocialId, String coordenadorId);
     List<Colaboracao> findByColaboradorId(String colabodadorId);
     List<Colaboracao> findByCoordenadorId(String coordenadorId);
